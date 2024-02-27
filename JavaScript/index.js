088b716959e1592de76dfaac1db6e06af7863dad
GiuliaLanari <giulia.lanari96@gmail.com>
2024-02-27 19:19:43 +0100

    sistemazione 1es

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,66 +1,35 @@
 const registrazioneKey = "registrazione-memoria";
-
+const h3 = document.querySelector("h3");
 const nameInput = document.getElementById("nome-utente");
-
 const btnSalvaInput = document.getElementById("btn-salva");
 const btnRimuoviInput = document.getElementById("btn-rimuovi");
 const form = document.querySelector("form");
 
-class registrazioneElement {
-  constructor(name) {
-    this.name = name;
-  }
-}
-
 const save = (e) => {
   e.preventDefault();
-  const newElement = new registrazioneElement(nameInput.value);
+  const nameInserito = nameInput.value;
+  const savedElements = localStorage.setItem(registrazioneKey, nameInserito);
 
-  const savedElements = localStorage.getItem(registrazioneKey);
-
-  if (savedElements) {
-    const savedElementsArray = JSON.parse(savedElements);
-    savedElementsArray.push(newElement);
-    localStorage.setItem(registrazioneKey, JSON.stringify(savedElementsArray));
-  } else {
-    const elements = [];
-    elements.push(newElement);
-    localStorage.setItem(registrazioneKey, JSON.stringify(elements));
-  }
-
-  nameInput.value = "";
-  generaTestoNomiPresenti();
+  rivela();
 };
 
-const generaTestoNomiPresenti = () => {
-  const savedElements = localStorage.getItem(registrazioneKey);
-  const lista = document.getElementById("add-testo");
-
-  if (savedElements) {
-    const savedElementsArray = JSON.parse(savedElements);
-    lista.innerHTML = "";
-
-    savedElementsArray.forEach((el) => {
-      const newLi = document.createElement("li");
-      newLi.className = "list-group-item";
-      newLi.innerText = el.name;
-
-      lista.appendChild(newLi);
-    });
-  }
-};
-
-const reset = () => {
-  const confermaElliminazione = confirm("Sei sicuro di voler elliminare?");
-
-  if (confermaElliminazione) {
+const rivela = () => {
+  const datoSalvato = localStorage.getItem(registrazioneKey);
+  if (datoSalvato) {
+    h3.innerText = datoSalvato;
     nameInput.value = "";
-    localStorage.removeItem(registrazioneKey);
+  } else {
+    h3.innerText = "";
   }
+};
+
+const rimuovi = (e) => {
+  localStorage.removeItem(registrazioneKey);
+  rivela();
 };
 
 window.onload = () => {
+  rivela();
   form.addEventListener("submit", save);
-  btnRimuoviInput.addEventListener("click", reset);
-  generaTestoNomiPresenti();
+  btnRimuoviInput.addEventListener("click", rimuovi);
 };
